06c8b4ffc82688f36552fb2ed541193e5558c181
lixiaofeng <1130034920@qq.com>
2019-09-03 21:31:02 +0800

    9-1 合并配置的设计与实现 - 需求分析 + 默认配置

---
+++ ts-axios/src/defaults.ts
@@ -0,0 +1,28 @@
+import { AxiosRequestConfig } from './types/index';
+
+const defaults: AxiosRequestConfig = {
+    method: 'get',
+
+    timeout: 0,
+
+    headers: {
+        common: {
+            Accept: 'application/json,text/plain,*/*'
+        }
+    }
+}
+
+// 请求参数没有data
+const methodsNoData = ['delete', 'get', 'head', 'options']
+methodsNoData.forEach((method) => {
+    defaults.headers[method] = {}
+})
+// 请求参数有data
+const methodsWithData = ['post', 'put', 'patch']
+methodsWithData.forEach((method) => {
+    defaults.headers[method] = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
+
+export default defaults;
