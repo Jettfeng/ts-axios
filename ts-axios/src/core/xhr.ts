aec3dea47fa30e0b9d0e868a933480d8cbd6ef9c
lixiaofeng <1130034920@qq.com>
2019-09-07 15:43:38 +0800

    11-1 withCredentials 需求分析&代码实现& demo 编写

--- ts-axios/src/core/xhr.ts
+++ ts-axios/src/core/xhr.ts
@@ -5,7 +5,7 @@ import { createError } from '../helpers/error';
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     // TODO
     return new Promise((resolve, reject) => {
-        const { data = null, url, method = "get", headers, responseType, timeout, cancelToken } = config
+        const { data = null, url, method = "get", headers, responseType, timeout, cancelToken, withCredentials } = config
         const request = new XMLHttpRequest()
         if (responseType) {
             request.responseType = responseType
@@ -44,6 +44,10 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         // 超时处理
         request.ontimeout = function handleTimeout() {
             createError(`Timeout of ${config.timeout} ms exceeded`, config, 'ECONNABORTED', request)
+        }
+        // 跨域处理
+        if (withCredentials) {
+            request.withCredentials = withCredentials
         }
         Object.keys(headers).forEach((name) => {
             if (data === null && name.toLowerCase() === 'content-type') {
