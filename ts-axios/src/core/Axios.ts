109361a1da97a6ad6853b7aba45651400e80f9a7
lixiaofeng <1130034920@qq.com>
2020-02-15 16:08:14 +0800

    [2020/02/15] 修改bug

--- ts-axios/src/core/Axios.ts
+++ ts-axios/src/core/Axios.ts
@@ -5,16 +5,16 @@ export default class Axios {
     request(config: AxiosRequestConfig): AxiosPromise {
         return dispatchRequest(config)
     }
-    get(url: string, config: AxiosRequestConfig): AxiosPromise {
+    get(url: string, config?: AxiosRequestConfig): AxiosPromise {
         return this._requestMethodWithoutData('get', url, config)
     }
-    delete(url: string, config: AxiosRequestConfig): AxiosPromise {
+    delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
         return this._requestMethodWithoutData('delete', url, config)
     }
-    head(url: string, config: AxiosRequestConfig): AxiosPromise {
+    head(url: string, config?: AxiosRequestConfig): AxiosPromise {
         return this._requestMethodWithoutData('head', url, config)
     }
-    options(url: string, config: AxiosRequestConfig): AxiosPromise {
+    options(url: string, config?: AxiosRequestConfig): AxiosPromise {
         return this._requestMethodWithoutData('options', url, config)
     }
     post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
